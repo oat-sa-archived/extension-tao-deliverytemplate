e60c4e5e26c8b2b2f2a8609bef78bb3025a08ec9
Joel Bout <joel@taotesting.com>
2015-01-23 20:03:07 +0100

    Moved constants from taoDelivery

---
+++ rdf/DeliveryContent.php
@@ -0,0 +1,37 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * 
+ */
+namespace oat\taoDeliveryTemplate\rdf;
+
+/**
+ * RDF constants
+ *
+ * @access public
+ * @author Joel Bout, <joel@taotesting.com>
+ * @package taoDeliveryTemplate
+ */
+class DeliveryContent
+{
+    const SUPER_CLASS = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryContentSuperClass';
+    
+    const CLASS_URI = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#AbstractDeliveryContent';
+    
+    const PROPERTY_IMPLEMENTATION = 'http://www.tao.lu/Ontologies/TAODelivery.rdf#DeliveryContentImplementation';
+    
+}
